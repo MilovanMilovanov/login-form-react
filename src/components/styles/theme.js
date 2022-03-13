691014063277cf45c116e5a0a1a4db4814814f44
MilovanMilovanov <ninapredninazad@gmail.com>
2022-03-13 20:59:56 +0200

    Update theme.js

--- src/components/styles/theme.js
+++ src/components/styles/theme.js
@@ -1,5 +1,5 @@
 const theme = {
-    primaryColor: 'rgb(121, 214, 210)', 
+    titleColor: 'rgb(121, 214, 210)', 
     primaryPadding: '1.2rem',
     primaryBorderRadius: '5px',
     primaryBoxShadow: '0 0 10px rgb(168, 216, 235)'
