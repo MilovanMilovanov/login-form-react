8809cb03fff0d38e2bf8815b792b0544da255ea0
MilovanMilovanov <ninapredninazad@gmail.com>
2022-03-13 21:06:08 +0200

    Update MatrixRainEffect.style.js

--- src/components/styles/MatrixRainEffect.style.js
+++ src/components/styles/MatrixRainEffect.style.js
@@ -7,15 +7,15 @@ const fadeIn = keyframes`
 
 // Some of the styles didn't work in the style component itself, that's why i put them in an object
 export const Wrapper = styled.div`
-${{
-        overflow: 'hidden',
-        position: 'absolute',
-        height: '100%',
-        width: '100%',
-        filter: 'blur(0px) brightness(150%)',
-        zIndex: -1,
-        left: '0',
-        top: '0',
-    }};
-animation: ${fadeIn} ${props => props.delay ? '6s' : '3s'};
+  animation: ${fadeIn} ${props => props.delay ? '6s' : '3s'};
+   ${{
+     filter: 'blur(0px) brightness(150%)',
+     position: 'absolute',
+     overflow: 'hidden',
+     height: '100%',
+     width: '100%',
+     zIndex: -1,
+     left: '0',
+     top: '0',
+   }};
 `;
