829ccf56c6ee55716d347a2f47650ea11af52180
MilovanMilovanov <ninapredninazad@gmail.com>
2022-03-13 21:01:35 +0200

    Update globalStyle.js

--- src/components/styles/globalStyle.js
+++ src/components/styles/globalStyle.js
@@ -1,24 +1,24 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-* {
-    margin: 0;
-    padding: 0;
-    border: none;
-    outline: 0;
-    appearance: none;
-    box-sizing: border-box;
-    z-index: 1;
-  }
+   * {
+     box-sizing: border-box;
+     appearance: none;
+     border: none;
+     outline: 0;
+     z-index: 1;
+     padding: 0;
+     margin: 0;
+   };
 
   body {
-    font-family: Arial, Helvetica, sans-serif;
-    display: flex;
+  font-family: Arial, Helvetica, sans-serif;
+    background: rgb(245, 241, 241);
     justify-content: center;
     padding: 1.2em;
+    display: flex;
     height: 100vh;
     width: 100%;
-    background: rgb(245, 241, 241);
   }`;
 
 export default GlobalStyles;
