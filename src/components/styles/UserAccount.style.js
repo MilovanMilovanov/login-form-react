2e704ea1c515d0aadc6c71854e1d9beb78112a8e
MilovanMilovanov <ninapredninazad@gmail.com>
2022-06-06 00:58:29 +0300

    Add files via upload

---
+++ src/components/styles/UserAccount.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Link = styled.p`
+        background: rgba(255,255,255, .5);
+        margin: 0 0 1.6em 0;
+        font-size: 1rem;
+        padding: 1rem;
+
+        :hover {
+            background: rgba(255,255,255, .7);
+            a {
+                color: rgb(0, 0, 255);
+            }
+        }
+    `;
