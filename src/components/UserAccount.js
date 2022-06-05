9db44f8681cfa9e66da5b98d6fd0efa46afdf37e
MilovanMilovanov <ninapredninazad@gmail.com>
2022-06-06 00:56:47 +0300

    Update UserAccount.js

--- src/components/UserAccount.js
+++ src/components/UserAccount.js
@@ -1,16 +1,24 @@
 import React from "react";
 
-import VoiceMessage from "./VoiceMessage";
-import Button from "./Button";
+import VoiceMessage from './VoiceMessage';
+import Button from './Button';
 import Title from './Title';
-import * as S from '../components/styles/Title.style';
+import * as S from './styles/UserAccount.style';
+
+const myGithubLinks = [
+    'https://MilovanMilovanov.github.io/CV.pdf',
+    'https://milovanmilovanov.github.io/Personal-FAQ-with-React'
+];
 
 function UserAccount({ user, logout }) {
     return (
         <>
             <VoiceMessage />
             <Title username={user.name} />
-            <Button logout={logout} />
+            {
+                myGithubLinks.map(link => <S.Link key={link}><a href={link} target='_SEJ' rel='noreferrer'>{link}</a></S.Link>)
+            }
+            < Button logout={logout} />
         </>
     )
 }
